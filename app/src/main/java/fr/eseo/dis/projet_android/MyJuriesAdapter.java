8e18bb7cc49a51fdf545d797f1512ee92a489107
charboni <32124641+charboni@users.noreply.github.com>
2018-01-17 14:43:55 +0100

    liste notes

--- app/src/main/java/fr/eseo/dis/projet_android/MyJuriesAdapter.java
+++ app/src/main/java/fr/eseo/dis/projet_android/MyJuriesAdapter.java
@@ -52,8 +52,8 @@ public class MyJuriesAdapter extends
         final Juries juries = myJuriesList.get(position);
 
         holder.juryId.setText("JURY "+String.valueOf(juries.getIdJury()));
-        holder.juryDate.setText("Date soutenance : "+juries.getDate().toString());
-        holder.juryDesc.setText("Membres : "+juries.getDescription());
+        holder.juryDate.setText("Date of presentation : "+juries.getDate().toString());
+        holder.juryDesc.setText("Members : "+juries.getDescription());
 
         holder.view.setOnClickListener(new View.OnClickListener() {
             @Override
